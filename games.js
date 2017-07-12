02f0823899e3e0118112fcc3d14b0d4cf201e1fa
oneill011990 <felix@kraas-family.de>
2017-07-12 21:44:59 +0200

    Added Crysis 2

--- games.js
+++ games.js
@@ -43,5 +43,6 @@ GAMES = [
       {name: 'Life is Strange', engine: UNREAL3, releaseDate: {d: 30, m: 1, y: 2015}, imgId: 'life-is-strange', yt: 'YznXuKwJtMg'},
       {name: 'The Forest', engine: UNITY, releaseDate: {d: 30, m: 5, y: 2014}, imgId: 'the-forest', yt: '4qTtVMM3uqQ'},
       {name: 'The Witcher 3', engine: RED_ENGINE, releaseDate: {d: 19, m: 5, y: 2015}, imgId: 'witcher3', yt: 'tDfa1Qp2SwA'},
-      {name: 'Crysis', engine: CRY_ENGINE, releaseDate: {d: 16, m: 11, y: 2007}, imgId: 'crysis', yt: 'q11NZDhv__A'} //Trailer auf English und Für PS3/XBOX360 Version des Spiels
+      {name: 'Crysis', engine: CRY_ENGINE, releaseDate: {d: 16, m: 11, y: 2007}, imgId: 'crysis', yt: 'q11NZDhv__A'}, //Trailer auf English und Für PS3/XBOX360 Version des Spiels
+      {name: 'Crysis 2', engine: CRY_ENGINE, releaseDate: {d: 24, m: 3, y: 2011}, imgId: 'crysis2', yt: 'dmGAfgv9uPo'}, //Trailer auf English
 ]
