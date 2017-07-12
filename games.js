e35369eed4d1e9b1acf6bc79316ecbdf884efb27
oneill011990 <felix@kraas-family.de>
2017-07-12 21:57:42 +0200

    Added Borderlands

--- games.js
+++ games.js
@@ -45,5 +45,6 @@ GAMES = [
       {name: 'The Witcher 3', engine: RED_ENGINE, releaseDate: {d: 19, m: 5, y: 2015}, imgId: 'witcher3', yt: 'tDfa1Qp2SwA'},
       {name: 'Crysis', engine: CRY_ENGINE, releaseDate: {d: 16, m: 11, y: 2007}, imgId: 'crysis', yt: 'eNDP-xLuOxM'}, //Trailer auf English
       {name: 'Crysis 2', engine: CRY_ENGINE, releaseDate: {d: 24, m: 3, y: 2011}, imgId: 'crysis2', yt: 'dmGAfgv9uPo'}, //Trailer auf English
-      {name: 'Crysis 3', engine: CRY_ENGINE, releaseDate: {d: 21, m: 2, y: 2013}, imgId: 'crysis3', yt: 'g5NNkyDt65w'} //Trailer auf English
+      {name: 'Crysis 3', engine: CRY_ENGINE, releaseDate: {d: 21, m: 2, y: 2013}, imgId: 'crysis3', yt: 'g5NNkyDt65w'}, //Trailer auf English
+      {name: 'Borderlands', engine: UNREAL3, releaseDate: {d: 30, m: 10, y: 2009}, imgId: 'borderlands', yt: 'DfR95_5v-hU'}
 ]
